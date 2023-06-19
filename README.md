449ce614695367363a47dbe1220d4213b60d6c49
Tanner <tannerhlyon@gmail.com>
2023-06-19 15:56:36 -0700

    quick setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Cover-Letter-Generator
-Easy Cover Letter Generator
+
+This is just a beginning template that I plan to add to. 
+
+At the moment, its a quick shitty algo that just generically allows you to plug some info into some arguments that will generate your cover letter. This works well for cover letters that you just type/copy-paste into a field; examples being 'Wellfound.com' and 'Ycombinator'. 
+
+Goal going forward will be to add a pdf output functionality. 
+
---
+++ cover-letter-algo.js
@@ -0,0 +1,27 @@
+const coverLetter = (position, company, article) => {
+    if(!position || !company || !article) return `Y'all, Check Yo Shit!`; 
+
+    let result = `
+
+Hi Team,
+
+I'm so excited to apply for the ${position} Software Engineer role at ${company}.
+
+My most recent role as a software engineer featured a tech stack that focused on React, Redux, NodeJS and postgreSQL. And our platform, Gimme, utilized React Hooks, NodeJS, Typescript, TDD, and MongoDB, in an application/chrome extension that executed extensive API calls via parsing through very large datasets.
+
+From reading the ${company} Engineering Blog (particularly, '${article}'), it's extremely clear that ${company}'s technical foundation is rooted in not only engineering best practices, but in true engineering excellence and innovation (Congratulations on your recent integration with Amazon’s Buy With Prime!) - and this is the core reason why I'd be so excited to bring my own abilities to the team there. 
+
+I hope we have a chance to connect. If there's a time and day that works well for you, I'll absolutely make it work on my end.
+
+All the best, 
+
+Tanner Lyon 
+Software Engineer
+`; 
+return result; 
+}
+
+const company = 'Whippy'; 
+const article = `Though the articles are temporary as they quote Fredrick Douglas, I understand what the companies approach to web development is`; 
+const position = 'Frontend'
+console.log(coverLetter(position, company, article))
